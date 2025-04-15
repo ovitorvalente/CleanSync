71eadb4357d1230b68515012d4c0c33520fbe575
ovitorvalente <ovitorvalente@gmail.com>
2025-04-14 22:28:54 -0300

    feat: add home app layout and home function with alert

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,24 +1,46 @@
-﻿using System.Text;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
+﻿using System.Windows;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace CleanSync
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void RunCleanDB(object sender, MouseButtonEventArgs e)
+        {
+            MessageBoxResult result = MessageBox.Show(
+                @"ATENÇÃO!
+Essa operação é irreversível.
+Você está prestes a executar um processo crítico de limpeza de dados no banco de dados atualmente conectado.
+Certifique-se de que este banco é o que será utilizado pelo Bridge, pois todas alterações realizadas não poderão ser desfeitas.
+
+Deseja realmente continuar?
+
+Confirmação de Operação Crítica
+                ",
+                
+                "Leia com atenção", 
+                MessageBoxButton.YesNo, 
+                MessageBoxImage.Warning
+            );
+
+            if (result != MessageBoxResult.Yes)
+            {
+                MessageBox.Show("Operacao cancelada");
+                return;
+            }
+
+            MessageBox.Show("Iniciando Operacao");
+            ClearDatabase();
+        }
+
+        private void ClearDatabase()
+        {
+
         }
     }
 }
